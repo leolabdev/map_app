1a81f4259bff52c0786ab6f0fd895b8b6bcee77a
Mikhail <mikko.deriabin@gmail.com>
2022-02-23 17:52:30 +0200

    add leaflet library and map to index.html

--- js/map.js
+++ js/map.js
@@ -1,2 +1,6 @@
 const mapSection = document.querySelector("#mapSection");
 const mapDiv = mapSection.querySelector("#map");
+
+const map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
