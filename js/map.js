dc8745a3428917240665d030ca3d4524fd788217
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2022-02-28 16:09:48 +0200

    remove bag code

--- js/map.js
+++ js/map.js
@@ -3,13 +3,8 @@ const mapDiv = mapSection.querySelector("#map");
 
 const map = L.map('map').setView([60.169, 24.938], 13);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
-
-L.Routing.control({
-    waypoints: [
-        L.latLng(60.169, 24.938),
-        L.latLng(60.9827, 25.6612),
-        L.latLng(62.242, 25.747)
-    ]
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+    maxZoom: 18,
 }).addTo(map);
+
 
