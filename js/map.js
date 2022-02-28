c0c4033c8d6f73bb73fb764c6168ff2d1d57c46d
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2022-02-28 15:13:51 +0200

    test points and routes added to map

--- js/map.js
+++ js/map.js
@@ -4,3 +4,11 @@ const mapDiv = mapSection.querySelector("#map");
 const map = L.map('map').setView([51.505, -0.09], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+L.Routing.control({
+    waypoints: [
+        L.latLng(51.505, -0.09),
+        L.latLng(52.505, -0.09),
+        L.latLng(53.505, -1)
+    ]
+}).addTo(map);
