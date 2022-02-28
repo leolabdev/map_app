b6a0c5d2dd83c28294f7da89a114d730c63c210c
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2022-02-28 15:40:50 +0200

    coordinates updated

--- js/map.js
+++ js/map.js
@@ -1,14 +1,15 @@
 const mapSection = document.querySelector("#mapSection");
 const mapDiv = mapSection.querySelector("#map");
 
-const map = L.map('map').setView([51.505, -0.09], 13);
+const map = L.map('map').setView([60.169, 24.938], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
 L.Routing.control({
     waypoints: [
-        L.latLng(51.505, -0.09),
-        L.latLng(52.505, -0.09),
-        L.latLng(53.505, -3)
+        L.latLng(60.169, 24.938),
+        L.latLng(60.9827, 25.6612),
+        L.latLng(62.242, 25.747)
     ]
 }).addTo(map);
+
