54055d9dc29582ae0fd44f4a1c907ac6c5e873df
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2022-03-07 16:25:59 +0200

    test drop listes added

---
+++ react-map-js/src/droplist1.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+
+
+
+
+const DropList1 = () => {
+    const options = [
+        { label: 'Lahti', value: 'Lahti' },
+        { label: 'Helsinki', value: 'Helsinki' },
+        { label: 'Tampere', value: 'Tampere' },
+    ];
+
+    const [value, setValue] = React.useState('Lahti');
+
+    const handleChange = (event) => {
+        setValue(event.target.value);
+    };
+
+    return (
+            <>
+                <select value={value} onChange={handleChange}>
+                    {options.map((option) => (
+                        <option value={option.value}>{option.label}</option>
+                    ))}
+                </select>
+
+
+            <p>We go to {value}!</p>
+            </>
+    );
+};
+export default DropList1
