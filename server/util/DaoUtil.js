5f11bdac1fda716471466ce2907c002cb3c8b7ed
Mikhail <mikko.deriabin@gmail.com>
2022-03-09 14:52:57 +0200

    add DAO for Manufacturer class and update sql script

---
+++ server/util/DaoUtil.js
@@ -0,0 +1,15 @@
+class DaoUtil{
+    getDataValues(respArr){
+        if(respArr != null){
+            let result = [];
+            for(let i=0; i<respArr.length; i++){
+                result[i] = respArr[i].dataValues;
+            }
+            return result;
+        } else {
+            return null;
+        }
+    }
+}
+
+module.exports.DaoUtil = DaoUtil;
