8a22f20befdf36249d667947148648723279699d
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2022-03-20 17:22:35 +0200

    added clientUsername/manufacturerUsername support
    still need to fix->>>
    post request is almost done but there is still some problem with transferring data to PostNewHuman.js function ,

--- react-map-js/src/api/humans/PostNewHuman.js
+++ react-map-js/src/api/humans/PostNewHuman.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 export const postNewHuman = async (humansType, ...post) => {
 
-
+    // console.log("hello", ...post)
 
     try {
         // var request = {
@@ -19,44 +19,57 @@ export const postNewHuman = async (humansType, ...post) => {
         //         lon: post.lon
         //     }
         // }
+        var request = {
+            // username: post.username,
+            clientUsername: "post.dadclsientadasdUsername",
+            name: "post.naddsadaasdsame",
+            addressAdd: {
+                city: "post.city",
+                street: "posst.street",
+                building: "possst.building",
+                lat: "post.lan",
+                lon: "post.lon"
+            }
+        }
 
-        // if (humansType === "client") {
-        //     if (request.username) {
-        //         request.clientUsername = request.username;
-        //         delete request.username
-        //     }
-        //     else if (request.manufacturerUsername) {
-        //         request.clientusername = request.manufacturerUsername;
-        //         delete request.manufacturerUsername
-        //     }
-        // }
-        // else if (humansType === "manufacturer") {
-        //     if (request.username) {
-        //         request.manufacturerUsername = request.username;
-        //         delete request.username
-        //     }
+        if (humansType === "client") {
+            if (request.username) {
+                request.clientUsername = request.username;
+                delete request.username
+            }
+            else if (request.manufacturerUsername) {
+                request.clientUsername = request.manufacturerUsername;
+                delete request.manufacturerUsername
+            }
+        }
+        else if (humansType === "manufacturer") {
+            if (request.username) {
+                request.manufacturerUsername = request.username;
+                delete request.username
+            }
 
-        //     else if (request.clientusername) {
-        //         request.manufacturerUsername = request.clientusername;
-        //         delete request.clientusername
-        //     }
-        // }
+            else if (request.clientUsername) {
+                request.manufacturerUsername = request.clientUsername;
+                delete request.clientUsername
+            }
+        }
 
 
         // var resp = await axios.post(`http://localhost:3000/${humansType}/create`, {
         var resp = await axios.post(`http://localhost:8081/dao/${humansType}`, {
+            // var resp = await axios.post(`http://localhost:8081/dao/client/`, {
 
 
-            // request
-            ...post
-            // username: post.clientUsername,
-            // name: post.name,
+            ...request
+            // ...post
+            // clientUsername: "post.clientUsername",
+            // name: "post.name",
             // addressAdd: {
-            //     city: post.city,
-            //     street: post.street,
-            //     building: post.building,
-            //     lat: post.lan,
-            //     lon: post.lon
+            //     city: "post.city",
+            //     street: "post.street",
+            //     building: "post.building",
+            //     lat: "post.lan",
+            //     lon: "post.lon"
 
             //     // username: "hello",
             //     // name: "lol",
