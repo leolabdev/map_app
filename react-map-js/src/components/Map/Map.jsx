3496c98b86304b37cecbadb13dedf97a40987839
Leo Hannolainen <61798137+leolab1337@users.noreply.github.com>
2022-04-17 18:21:48 +0300

    code commented for removing in the future

--- react-map-js/src/components/Map/Map.jsx
+++ react-map-js/src/components/Map/Map.jsx
@@ -228,71 +228,71 @@ function Map({ coordinates, setCoordinates, LocationMarker, start, end, Leafletg
     //     )
     // }
 
-    function showRoute() {
-        // map = useMap()
-        console.log(orderPoints)
+    // function showRoute() {
+    //     // map = useMap()
+    //     console.log(orderPoints)
 
-        console.log("orders id in  map request", ordersIdForRoutes)
+    //     console.log("orders id in  map request", ordersIdForRoutes)
 
-        let sendOrdersIdForRoutesData={
-            orderIds: [
-                ...ordersIdForRoutes
-            ],
-            fuelusage: 5.7
-        }
+    //     let sendOrdersIdForRoutesData={
+    //         orderIds: [
+    //             ...ordersIdForRoutes
+    //         ],
+    //         fuelusage: 5.7
+    //     }
 
-        // let coordinatesData = {
-        //     coordinates: [
-        //         ...orderPoints
-        //         // [start.lng, start.lat],
-        //         // [start.lon, start.lat],
-        //         // // [end.lng, end.lat],
-        //         // [end.lon, end.lat],
-        //     ]
-        // }
-        // console.log(coordinatesData)
-        console.log(sendOrdersIdForRoutesData)
-        if (sendOrdersIdForRoutesData.orderIds.length !== 0) {
-            fetch('http://localhost:8081/api/v1/routing/orders', {
-                method: 'POST', // or 'PUT'
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(sendOrdersIdForRoutesData),
-            })
-                .then(response => response.json())
-                .then(data => {
-                    console.log('Success:', data);
-                    addRoute(data);
-                    // removeStartMarker();
-                    // removeEndMarker();
-                    // coordinatesData.coordinates.map((c)=>addOrderMarker(c[1],c[0]))
-                    console.log(data.features[0].properties.summary.orders[2])
-                    data.features[0].properties.summary.orders[2].forEach(
-                        (o)=>{
-                            addOrderMarker(o.deliveryAddress.lat,o.deliveryAddress.lon)
-                            addOrderMarker(o.shipmentAddress.lat,o.shipmentAddress.lon)
-                            console.log(o.orderId)
-                        }
-                    )
-                    // data.coordinates.map((c)=>addOrderMarker(c[1],c[0]))
+    //     // let coordinatesData = {
+    //     //     coordinates: [
+    //     //         ...orderPoints
+    //     //         // [start.lng, start.lat],
+    //     //         // [start.lon, start.lat],
+    //     //         // // [end.lng, end.lat],
+    //     //         // [end.lon, end.lat],
+    //     //     ]
+    //     // }
+    //     // console.log(coordinatesData)
+    //     console.log(sendOrdersIdForRoutesData)
+    //     if (sendOrdersIdForRoutesData.orderIds.length !== 0) {
+    //         fetch('http://localhost:8081/api/v1/routing/orders', {
+    //             method: 'POST', // or 'PUT'
+    //             headers: {
+    //                 'Content-Type': 'application/json',
+    //             },
+    //             body: JSON.stringify(sendOrdersIdForRoutesData),
+    //         })
+    //             .then(response => response.json())
+    //             .then(data => {
+    //                 console.log('Success:', data);
+    //                 addRoute(data);
+    //                 // removeStartMarker();
+    //                 // removeEndMarker();
+    //                 // coordinatesData.coordinates.map((c)=>addOrderMarker(c[1],c[0]))
+    //                 console.log(data.features[0].properties.summary.orders[2])
+    //                 data.features[0].properties.summary.orders[2].forEach(
+    //                     (o)=>{
+    //                         addOrderMarker(o.deliveryAddress.lat,o.deliveryAddress.lon)
+    //                         addOrderMarker(o.shipmentAddress.lat,o.shipmentAddress.lon)
+    //                         console.log(o.orderId)
+    //                     }
+    //                 )
+    //                 // data.coordinates.map((c)=>addOrderMarker(c[1],c[0]))
     
-                    // addStartMarker(coordinatesData.coordinates[0][1], coordinatesData.coordinates[0][0]);
-                    // addEndMarker(coordinatesData.coordinates[1][1], coordinatesData.coordinates[1][0]);
-                })  
-                .catch((error) => {
-                    console.error('Error:', error);
-                });
+    //                 // addStartMarker(coordinatesData.coordinates[0][1], coordinatesData.coordinates[0][0]);
+    //                 // addEndMarker(coordinatesData.coordinates[1][1], coordinatesData.coordinates[1][0]);
+    //             })  
+    //             .catch((error) => {
+    //                 console.error('Error:', error);
+    //             });
     
-            return (
-                null
-            )
-        }
-        else{
-            alert("plz select a order/orders")
-            return (null)
-        }
-    }
+    //         return (
+    //             null
+    //         )
+    //     }
+    //     else{
+    //         alert("plz select a order/orders")
+    //         return (null)
+    //     }
+    // }
 
         
        
