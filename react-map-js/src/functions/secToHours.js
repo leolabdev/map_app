04e9d9b24b148b2a859981701430a5a2fef926a4
Leo Hannolainen <61798137+leolab1337@users.noreply.github.com>
2022-04-18 18:30:18 +0300

    added transform function for  more detaled summary output

---
+++ react-map-js/src/functions/secToHours.js
@@ -0,0 +1,8 @@
+const secToHours = (sec) => {
+    const h = sec/3600 ^ 0 ;
+    const m = (sec-h*3600)/60 ^ 0 ;
+    const s = (sec-h*3600-m*60).toFixed(0) ;
+    return (h<10?"0"+h:h)+" h. "+(m<10?"0"+m:m)+" min. "+(s<10?"0"+s:s)+" sec." 
+}
+
+export default secToHours
