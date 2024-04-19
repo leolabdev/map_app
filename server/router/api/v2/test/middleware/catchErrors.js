8c5fd8b36b80d6c5507c015ae5d2cb6924f644fb
MikhailDeriabin <mikko.deriabin.work@gmail.com>
2024-04-19 21:21:51 +0300

    add pipe error handler

---
+++ server/router/api/v2/test/middleware/catchErrors.js
@@ -0,0 +1,21 @@
+export const catchErrors = (err, req, res, next) => {
+    const {respErrorFieldName} = res;
+
+    if(!respErrorFieldName){
+        const metaDataError = new Error('Please check that the addMetaData middleware is applied to the pipe. ' +
+            'The middleware provides meta information required by catchErrors middleware');
+        res[respErrorFieldName] = [err, metaDataError];
+        return next();
+    }
+
+    let previousErrors = [];
+    if(Array.isArray(res[respErrorFieldName]))
+        previousErrors = res[respErrorFieldName];
+
+    if(Array.isArray(err))
+        res[respErrorFieldName] = [...previousErrors, ...err];
+    else
+        res[respErrorFieldName] = [...previousErrors, err];
+
+    next();
+};
