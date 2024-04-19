b08ff79fe5a27d4cb531c257a219c89e6dd6bafe
MikhailDeriabin <mikko.deriabin.work@gmail.com>
2024-04-19 21:23:25 +0300

    add wrapper for controller function

---
+++ server/router/api/v2/test/util/addController.js
@@ -0,0 +1,15 @@
+import {createAsyncHandler} from "./createAsyncHandler.js";
+
+/**
+ *
+ * @param {(req: Request, res: Response) => Promise<any>} controllerFn function, which gets req and res objects
+ * and returns the result of the request. If there is an error, it throws it
+ * @returns {function(*, *, *): Promise<Awaited<*>>}
+ */
+export function addController(controllerFn) {
+    return createAsyncHandler(async function(req, res, next) {
+        const {respFieldName} = res;
+        res[respFieldName] = await controllerFn(req, res);
+        next();
+    })
+}
