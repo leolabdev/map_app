fabeef4440adf69b0dbc058cc9ea4f32a97338f2
MikhailDeriabin <mikko.deriabin.work@gmail.com>
2024-04-19 21:22:21 +0300

    add response formatter

---
+++ server/router/api/v2/test/middleware/formatResponse.js
@@ -0,0 +1,17 @@
+export const formatResponse = (req, res) => {
+    const {respFieldName, respErrorFieldName} = res;
+
+    if(!respFieldName || !respErrorFieldName)
+        throw new Error('Please check that the addMetaData middleware is applied to the pipe. ' +
+            'The middleware provides meta information required by formatResponse middleware');
+
+    if(res[respErrorFieldName] && Array.isArray(res[respErrorFieldName]) && res[respErrorFieldName].length > 0)
+        res[respErrorFieldName] = res[respErrorFieldName];
+    else if(res[respErrorFieldName])
+        res[respErrorFieldName] = [res[respErrorFieldName]];
+
+    res.json({
+        [respFieldName]: res[respFieldName] || null,
+        [respErrorFieldName]: res[respErrorFieldName] || null
+    });
+};
