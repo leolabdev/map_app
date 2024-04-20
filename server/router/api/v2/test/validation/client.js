71da914490a90c42d5c543f060eb339d5ae6cae9
MikhailDeriabin <mikko.deriabin.work@gmail.com>
2024-04-20 18:45:21 +0300

    reorganize files

---
+++ server/router/api/v2/test/validation/client.js
@@ -0,0 +1,24 @@
+import Joi from "joi";
+
+/**
+ *
+ * @type {
+ * [{ schema: SchemaMap<any, false>, location?: 'body' | 'query' | 'param' }] |
+ * { schema: SchemaMap<any, false>, location?: 'body' | 'query' | 'param' }
+ * }
+ */
+export const clientCreate = [
+    {
+    schema: {
+        clientUsername: Joi.string().required(),
+        name: Joi.string().optional(),
+        addressId: Joi.number().integer().optional()
+    }
+},
+    {
+        schema: {
+            search: Joi.string().required()
+        },
+        location: 'query'
+    }
+]
