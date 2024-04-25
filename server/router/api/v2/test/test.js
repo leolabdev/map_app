2b4a198051081ff29756a2a6572d6e4842608d3d
MikhailDeriabin <mikko.deriabin.work@gmail.com>
2024-04-25 21:33:06 +0300

    add authentication test

--- server/router/api/v2/test/test.js
+++ server/router/api/v2/test/test.js
@@ -4,20 +4,25 @@ import {clientCreate} from "./validation/client.js";
 import {APIError} from "../../../../util/error/APIError.js";
 import {ErrorReason} from "../../../../util/error/ErrorReason.js";
 import {RouteBuilder} from "./util/pipeline/RouteBuilder.js";
+import {Method} from "./util/pipeline/Method.js";
 
 const router = express.Router();
 
-// Asynchronous controller
-const clientController = async (req, res) => {
-    if(req.body['lol'])
-        throw new APIError(ErrorReason.NOT_FOUND, 'upsis, just testing', req.baseUrl, "lol");
-
-    return { message: "Client successfully processed" };
-};
-new RouteBuilder('/', 'post')
+new RouteBuilder('/', Method.POST)
+    .authenticate()
     .serializeReq(ClientReq).serializeRes(ClientRes)
     .validate(clientCreate)
     .addController(clientController).attachToRouter(router);
+
+async function clientController(req, res) {
+    if(req.body['lol'])
+        throw new APIError(ErrorReason.NOT_FOUND, 'upsis, just testing', req.baseUrl, 'lol');
+
+    const {authFieldName} = req;
+
+    return { message: 'Client successfully processed', loggedUser: req[authFieldName] };
+}
+
 
 
 //router.post('/', serializeReq(ClientReq), validate(client), addController(clientController), serializeRes(ClientRes));
