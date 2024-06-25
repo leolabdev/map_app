fca16218a21b1310d74b56fe4ad5ba9d26d7abe6
MikhailDeriabin <mikko.deriabin.work@gmail.com>
2024-06-25 17:25:04 +0300

    update service validation with new specs

--- server/service/validation/order.js
+++ server/service/validation/order.js
@@ -2,10 +2,8 @@ import Joi from "joi";
 
 export const orderCreate = {
     schema: Joi.object({
-        manufacturerId: Joi.number().required(),
-        clientId: Joi.number().required(),
-        shipmentAddressId: Joi.number().required(),
-        deliveryAddressId: Joi.number().required(),
+        senderId: Joi.number().required(),
+        recipientId: Joi.number().required()
     })
 };
 
@@ -17,9 +15,7 @@ export const orderIds = {
 export const orderUpdate = {
     schema: Joi.object({
         id: Joi.number().required(),
-        manufacturerId: Joi.number(),
-        clientId: Joi.number(),
-        shipmentAddressId: Joi.number(),
-        deliveryAddressId: Joi.number(),
+        senderId: Joi.number(),
+        recipientId: Joi.number()
     })
 };
