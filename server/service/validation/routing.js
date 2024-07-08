31ef46a58979fd8f50ee11bb835218342dee331b
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-07-08 17:55:54 +0300

    add validation for RoutingService

---
+++ server/service/validation/routing.js
@@ -0,0 +1,25 @@
+import Joi from "joi";
+
+export const routingCoordinates = {
+    schema: Joi.object({
+        coordinates: Joi.array().items(
+            Joi.array().items(
+              Joi.number().min(0).max(180).required(),
+              Joi.number().min(0).max(90).required() 
+            ).length(2))
+        .required(),
+
+        startCoordinateIndex: Joi.number(),
+        endCoordinateIndex: Joi.number(),
+        avoidCityCenters: Joi.boolean(),
+        cityCentersToAvoid: Joi.array().items(Joi.string()),
+        isTrafficSituation: Joi.boolean(),
+    })
+};
+
+export const routingOrders = {
+    schema: Joi.object({
+        lon: Joi.number().required(),
+        lat: Joi.number().required()
+    })
+};
