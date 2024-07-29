de9f108993a054da7dd1f1aff6291355844e00ea
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-07-29 18:27:05 +0300

    clear the DB after each test

--- server/test/test_utils/connect-to-db.js
+++ server/test/test_utils/connect-to-db.js
@@ -1,8 +1,32 @@
 import SequelizeUtil from "../../modules/SequelizeUtil";
- 
+import * as mariadb from 'mariadb';
 
 beforeAll(async () => {
     const isConnected = await SequelizeUtil.isSequelizeConnected();
-
     expect(isConnected).toBe(true);
+});
+
+beforeEach(async () => {
+    const dbName = process.env.DATABASE;
+
+    const connection = await mariadb.createConnection({
+        host: process.env.DATABASE_HOST,
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: dbName
+    });
+    
+    const tables = await connection.query("SHOW TABLES");
+    const tableNames = tables
+        .filter(row => {
+            const tableName = row[`Tables_in_${dbName}`];
+            return tableName && !['Area', 'TMS'].includes(tableName);
+        })
+        .map(row => row[`Tables_in_${dbName}`]);
+
+    for (const tableName of tableNames) {
+        await connection.execute(`DELETE FROM \`${tableName}\``);
+    }
+
+    await connection.end();
 });
