94b6f0fc485b141d5d0e7654f4bd48f889e4d782
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-08-20 16:24:09 +0300

    add formatResponse() unit tests

---
+++ server/test/router/api/v2/routerBuilder/core/pipelineHandlers/formatResponse.test.js
@@ -0,0 +1,46 @@
+import { formatResponse } from "../../../../../../../router/api/v2/routeBuilder/core/pipelineHandlers/formatResponse";
+import { notBooleanAPIError, notFoundAPIError } from "../../../../../../test_utils/data/apiErrors";
+import { profile1 } from "../../../../../../test_utils/data/profiles";
+
+describe('formatResponse() test suite', () => {
+    const respFieldName = 'data';
+    const respErrorFieldName = 'errors';
+    const successStatus = 201;
+    const returnedData = profile1;
+    const returnedErrors = [notBooleanAPIError, notFoundAPIError];
+
+    const reqMock = {
+        
+    }
+    const resMock = {
+        [respFieldName]: returnedData,
+        [respErrorFieldName]: returnedErrors,
+        json: jest.fn()
+    }
+    const formatResponseHandler = formatResponse(respFieldName, respErrorFieldName, successStatus);
+
+    it('Should call res.json() with formatted response object in valid form', () => {
+        formatResponseHandler(reqMock, resMock);
+
+        expect(resMock.json).toHaveBeenCalledTimes(1);
+        expect(resMock.json).toHaveBeenCalledWith({
+            [respFieldName]: returnedData,
+            [respErrorFieldName]: returnedErrors
+        });
+    });
+
+    it('Should set the provided status code in res object "statusCode" field', () => {
+        formatResponseHandler(reqMock, resMock);
+
+        expect(resMock.statusCode).toEqual(successStatus);
+    });
+
+    it('Should remove "respFieldName", "respErrorFieldName", "metadataFieldName" from res object after execution', () => {
+        formatResponseHandler(reqMock, resMock);
+
+        expect(resMock).not.toEqual(expect.objectContaining({
+            [respFieldName]: expect.anything(),
+            [respErrorFieldName]: expect.anything()
+        }));
+    });
+});
