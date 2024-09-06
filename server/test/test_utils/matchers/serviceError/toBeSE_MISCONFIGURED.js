5327abe8f7b6cd481115f164b38470413a8915a8
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-09-06 13:51:48 +0300

    add toBeSE_MISCONFIGURED() jest matcher

---
+++ server/test/test_utils/matchers/serviceError/toBeSE_MISCONFIGURED.js
@@ -0,0 +1,24 @@
+import { SEReason } from "../../../../router/api/v2/routeBuilder/core/service/dataExtractors/error/SEReason";
+import { ServiceError } from "../../../../router/api/v2/routeBuilder/core/service/dataExtractors/error/ServiceError";
+import MatcherReturner from "../../jest_util/MatcherReturner";
+import { isErrorWithReason } from "./isErrorWithReason";
+
+/**
+ * Jest matcher checks whenever provided param is ServiceError with reason MISCONFIGURED
+ * @param {*} object object to check
+ * @returns {{ message: () => string, pass: boolean }}
+ */
+export function toBeSE_MISCONFIGURED(object) {
+    const returner = new MatcherReturner({received: object, utils: this.utils});
+    
+    if(!(object instanceof ServiceError))
+        return returner.passFalse('Received object is not of type ServiceError');
+
+    const isValid = isErrorWithReason(object, SEReason.MISCONFIGURED);
+
+    return isValid ?
+        returner.passTrue('Expected to not receive a ServiceError with reason MISCONFIGURED') :
+        returner.passFalse('Expected to receive ServiceError with reason MISCONFIGURED');
+}
+
+expect.extend({toBeSE_MISCONFIGURED});
