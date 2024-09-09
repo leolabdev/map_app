81ba4b08a4aa38febae750cbbafa759a6755fb8a
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-09-09 15:05:58 +0300

    change toBeSE() jest matcher to use SEReason as a param instead of actual ServiceError

--- server/test/test_utils/matchers/serviceError/individual/toBeSE.js
+++ server/test/test_utils/matchers/serviceError/individual/toBeSE.js
@@ -1,24 +1,25 @@
-import { SERVICE_ERROR_TYPE_NAME } from "../../../../../router/api/v2/routeBuilder/core/config";
+import { SEReason } from "../../../../../router/api/v2/routeBuilder/core/service/dataExtractors/error/SEReason";
 import { ServiceError } from "../../../../../router/api/v2/routeBuilder/core/service/dataExtractors/error/ServiceError";
 import MatcherReturner from "../../../jest_util/MatcherReturner";
 import { isErrorWithReason } from "../isErrorWithReason";
+import isSEReason from "../isSEReason";
 
 /**
- * Jest matcher checks whenever object is the provided ServiceError or not.
+ * Jest matcher checks whenever object has the provided ServiceError reason or not.
  * @param {*} received object to check
- * @param {ServiceError} expected expected ServiceError
+ * @param {SEReason} expected expected ServiceError reason
  * @returns {{ message: () => string, pass: boolean }}
  */
 export function toBeSE(received, expected) {
-    if(!expected || expected.type !== SERVICE_ERROR_TYPE_NAME.description)
-        throw new TypeError('The expected value must be of type ServiceError');
+    if(!isSEReason(expected))
+        throw new TypeError('The expected value must be of type SEReason');
 
     const returner = new MatcherReturner({received, utils: this.utils, expected});
     
     if(!(received instanceof ServiceError))
         return returner.passFalse('Received object is not of type ServiceError');
 
-    const isValid = isErrorWithReason(received, expected.reason);
+    const isValid = isErrorWithReason(received, expected);
 
     return isValid ?
         returner.passTrue(`Expected to not receive a ServiceError with reason ${expected.reason}`) :
--- server/test/test_utils/test/matchers/serviceError/individual/toBeSE.test.js
+++ server/test/test_utils/test/matchers/serviceError/individual/toBeSE.test.js
@@ -4,43 +4,40 @@ import passJestThis from "../../../../jest_util/passJestThisObject";
 import { toBeSE } from "../../../../matchers/serviceError/individual/toBeSE";
 
 describe('toBeSE() test suite', () => {
-    it('Should return object with field "pass" set to true if the param is expected ServiceError', () => {
-        const expectedError = new ServiceError({ reason: SEReason.LIMIT_EXCEEDED });
-        const receivedError = new ServiceError({ reason: SEReason.LIMIT_EXCEEDED });
+    it('Should return object with field "pass" set to true if the param has an expected SE reason', () => {
+        const expectedReason = SEReason.LIMIT_EXCEEDED;
+        const receivedError = new ServiceError({ reason: expectedReason });
 
-        const resp = passJestThis(toBeSE)(receivedError, expectedError);
+        const resp = passJestThis(toBeSE)(receivedError, expectedReason);
 
         expect(resp.pass).toBeTruthy();
     });
 
-    it('Should return object with field "pass" set to false if the param is not expected ServiceError', () => {
-        const expectedError = new ServiceError({ reason: SEReason.LIMIT_EXCEEDED });
+    it('Should return object with field "pass" set to false if the param has an expected SE reason', () => {
         const receivedError = new ServiceError({ reason: SEReason.NOT_ALLOWED });
 
-        const resp = passJestThis(toBeSE)(receivedError, expectedError);
+        const resp = passJestThis(toBeSE)(receivedError, SEReason.LIMIT_EXCEEDED);
 
         expect(resp.pass).toBeFalsy();
     });
 
     it('Should return object with field "pass" set to false if the param is not a ServiceError', () => {
-        const expectedError = new ServiceError({ reason: SEReason.LIMIT_EXCEEDED });
-
-        const resp = passJestThis(toBeSE)('not object', expectedError);
+        const resp = passJestThis(toBeSE)('not object', SEReason.LIMIT_EXCEEDED);
 
         expect(resp.pass).toBeFalsy();
     });
 
-    it('Should throw TypeError if the expected value is not of type ServiceError', () => {
+    it('Should throw TypeError if the expected value is not of type SEReason', () => {
         const receivedError = new ServiceError({ reason: SEReason.NOT_ALLOWED });
 
-        expect(() =>  passJestThis(toBeSE)(receivedError, 'not object')).toThrow(new TypeError("The expected value must be of type ServiceError"));
+        expect(() => passJestThis(toBeSE)(receivedError, 'not object')).toThrow(new TypeError("The expected value must be of type SEReason"));
     });
 
     it('Should be properly registered as a custom jest matcher', () => {
-        const validError = new ServiceError({ reason: SEReason.UNEXPECTED });
-        const otherError = new ServiceError({ reason: SEReason.NOT_ALLOWED });
+        const unexpectedError = new ServiceError({ reason: SEReason.UNEXPECTED });
+        const notAllowedError = new ServiceError({ reason: SEReason.NOT_ALLOWED });
 
-        expect(validError).toBeSE(validError);
-        expect(otherError).not.toBeSE(validError);
+        expect(unexpectedError).toBeSE(SEReason.UNEXPECTED);
+        expect(notAllowedError).not.toBeSE(SEReason.UNEXPECTED);
     });
 });
